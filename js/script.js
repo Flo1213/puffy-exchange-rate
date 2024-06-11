1422c291fdb8671a85aa9c11492717cf04b9a78a
Flo1213 <93582542+Flo1213@users.noreply.github.com>
2024-06-11 12:17:20 +0800

    Upgrade with add and delete features

--- js/script.js
+++ js/script.js
@@ -1,144 +1,216 @@
 "use strict";
-// https://restcountries.com/#endpoints-currency
+// https://www.exchangerate-api.com/
 
-// API KEY:b373a9c11a3542a45f331425
-
-import { countryCurrencyArray } from "./data.js";
+// Variables
+let countries = {};
+let matchedData = [];
 
 // SELECT ELEMENTS
-const countryCodes = document.querySelectorAll(".country__code");
-const countryInputs = document.querySelectorAll(".country__input");
-const keyboard = document.querySelector(".keyboard");
+const countryAll = document.querySelectorAll(".country");
+const container = document.querySelector(".container");
+const countryIcons = document.querySelectorAll(".country__icon");
 
-/* const fetchCountries = async function () {
+const btn = document.querySelector(".btn");
+
+const options = document.querySelector(".options");
+const optionItems = document.querySelectorAll(".options__item");
+
+// Fetching countries data
+
+const fetchCountries = async function () {
   try {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
 
-    const countries = data.map((country) => ({
+    countries = data.map((country) => ({
       countryName: country.name.common,
       currency: country.currencies,
+      flag: country.flags,
     }));
-    console.log(countries);
-    console.log(data[1]);
+
+    matchingData();
+    displayCountriesFlags();
+    displayCurrencyCode();
+    setElAttribute();
   } catch (error) {
     console.error(`Error fetching data of countries: ${error} `);
   }
-}; 
+};
 
-fetchCountries();*/
-
-let targetCurrency = {};
-let otherCurrency = {};
-
-const fetchData = async function (targetCountryId) {
+// Fetching currency rate data
+const fetchData = async function (country) {
   try {
     const res = await fetch(
-      `https://v6.exchangerate-api.com/v6/b373a9c11a3542a45f331425/latest/${targetCountryId}`
+      `https://v6.exchangerate-api.com/v6/b373a9c11a3542a45f331425/latest/${country}`
     );
     const data = await res.json();
-    // console.log(data);
+    console.log(data);
     const conversionRate = data.conversion_rates;
     return conversionRate;
   } catch (error) {
     console.error(error);
   }
 };
+
 // *************Functions******************
-const displayCurrencyCode = function () {
-  countryCodes.forEach((countryCode) => {
-    const countryName = countryCode
-      .closest(".country")
-      .querySelector(".country__name").textContent;
 
-    const matchedData = countryCurrencyArray.find(
-      (countryData) => countryData.country === countryName
-    );
+// Countries Related Functions
 
-    if (matchedData) {
-      countryCode.textContent = matchedData.currencyCode;
-    }
-  });
-};
+const matchingData = function () {
+  const countryNames = document.querySelectorAll(".country__name");
 
-const displayCurrencyRate = async function (e) {
-  try {
-    const targetCountryName = e.target.id;
-    const targetCountry = countryCurrencyArray.find((countryData) => {
-      return countryData.country === targetCountryName;
+  for (const entry of Object.entries(countries)) {
+    countryNames.forEach((country, index) => {
+      if (country.innerHTML === entry[1].countryName) {
+        matchedData[index] = entry[1];
+      }
     });
-
-    targetCurrency = {
-      country: targetCountry.country,
-      currencyCode: targetCountry.currencyCode,
-    };
-
-    // Find other countries
-    const otherCountries = Array.from(countryInputs)
-      .filter((input) => input !== targetCountryName)
-      .map((input) => input.id);
-
-    const otherCountriesId = getOthersId(otherCountries);
-
-    otherCurrency = Object.assign(
-      {},
-      { country: otherCountries, currencyCode: otherCountriesId }
-    );
-
-    // Fetch target currency data
-    const targetRate = await fetchData(targetCountry.currencyCode);
-
-    otherCurrency.conversionRate = otherCurrency.currencyCode.map(
-      (currency) => targetRate[currency]
-    );
-
-    displayOtherInput(otherCurrency, e);
-
-    // Find others currency
-  } catch (error) {
-    console.error(error);
   }
 };
 
-const getOthersId = function (otherCountries) {
-  const otherCountriesId = otherCountries.map(
-    (otherCountry) =>
-      countryCurrencyArray.find((item) => item.country === otherCountry)
-        .currencyCode
-  );
-  return otherCountriesId;
+const displayCountriesFlags = function () {
+  const countryFlags = document.querySelectorAll(".country__flag");
+
+  countryFlags.forEach((flag, index) => {
+    flag.innerHTML = `<img src="${matchedData[index].flag.svg}" alt="${matchedData[index].flag.alt} flag">`;
+  });
 };
 
-const displayOtherInput = function (otherCurrency, e) {
-  const targetValue = e.target.value;
-  console.log(targetValue);
-  countryInputs.forEach((countryInput) => {
-    const currencyIndex = otherCurrency.country.indexOf(countryInput.id);
-    if (currencyIndex !== -1) {
-      countryInput.value = Math.round(
-        targetValue * otherCurrency.conversionRate[currencyIndex]
-      );
-      countryInput.style.color = "#ffffff";
+const displaySideBar = function () {
+  const visibility = options.getAttribute("data-visible");
+  if (visibility === "false") {
+    options.setAttribute("data-visible", "true");
+  } else if (visibility === "true") {
+    options.setAttribute("data-visible", "false");
+  }
+};
+
+const displayCurrencyCode = function () {
+  const countryCodes = document.querySelectorAll(".country__code");
+  const matchCurrency = [];
+  matchedData.forEach((data, index) => {
+    matchCurrency[index] = Object.keys(data.currency);
+  });
+  countryCodes.forEach((countryCode, index) => {
+    countryCode.textContent = matchCurrency[index];
+  });
+};
+
+const getInputs = async function (e) {
+  try {
+    const countryInputs = document.querySelectorAll(".country__input");
+
+    const targetCurrency = e.target.getAttribute("data-currency");
+    console.log(e.target);
+
+    const targetValue = e.target.value;
+    const fetchTargetData = await fetchData(targetCurrency);
+    console.log(fetchTargetData);
+
+    countryInputs.forEach((input) => {
+      if (input.getAttribute("data-currency") !== targetCurrency) {
+        const dataCurrency = input.getAttribute("data-currency");
+        const conversionRate = fetchTargetData[dataCurrency];
+
+        input.value = (targetValue * conversionRate).toFixed(2);
+      }
+
+      if (targetValue === "") {
+        input.value = "";
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const setElAttribute = function () {
+  const countryInputs = document.querySelectorAll(".country__input");
+
+  countryInputs.forEach((country, index) => {
+    if (matchedData[index].countryName === country.id) {
+      const currencyKeys = Object.keys(matchedData[index].currency);
+      country.setAttribute("data-currency", currencyKeys);
+    } else {
+      console.error(`No matched data found for index ${index}`);
     }
   });
 };
 
-/* const displayKeyboard = function (e) {
-  const rect = e.target.getBoundingClientRect();
-  console.log(rect);
-  keyboard.style.opacity = 1;
-  console.log(rect.top);
-  keyboard.style.top = `${rect.top + 30}px`;
-};
- */
-// *************Event Handler******************
-document.addEventListener("DOMContentLoaded", function () {
+const addNewCurrency = function (e) {
+  const target = e.target.innerHTML;
+  const targetItem = e.target;
+
+  const html = `
+                   <div class="country">
+          <div class="country__info">
+            <div class="country__flag"></div>
+            <div class="country__name">${target}</div>
+          </div>
+          <div class="country__currency">
+            <input
+              type="text"
+              class="country__input"
+              id="${target}"
+              inputmode="numeric"
+            />
+            <p class="country__code"></p>
+            <ion-icon class="country__icon" name="trash-outline"></ion-icon>
+          </div>
+        </div>
+  `;
+  // Add new currency into currency board
+  container.insertAdjacentHTML("beforeend", html);
+
+  console.log(matchedData);
+  matchingData();
+  displayCountriesFlags();
   displayCurrencyCode();
+  displaySideBar();
+  removeNewCurrency(targetItem);
+  setElAttribute();
+  bindEventListeners();
+};
+
+const removeNewCurrency = function (targetItem) {
+  targetItem.remove();
+};
+
+const deleteCurrency = function (e) {
+  e.target.closest(".country").remove();
+  bindEventListeners();
+};
+
+// *************Event Handler******************
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchCountries();
+  bindEventListeners();
 });
 
-countryInputs.forEach((countryInput) => {
-  countryInput.addEventListener("input", function (e) {
-    e.preventDefault();
-    displayCurrencyRate(e);
+const bindEventListeners = function () {
+  const countryInputs = document.querySelectorAll(".country__input");
+
+  countryInputs.forEach((countryInput) => {
+    countryInput.addEventListener("input", function (e) {
+      e.preventDefault();
+      getInputs(e);
+    });
   });
+};
+
+btn.addEventListener("click", function () {
+  displaySideBar();
 });
+
+optionItems.forEach((optionItem) =>
+  optionItem.addEventListener("click", function (e) {
+    addNewCurrency(e);
+  })
+);
+
+countryIcons.forEach((icon) =>
+  icon.addEventListener("click", function (e) {
+    deleteCurrency(e);
+  })
+);
